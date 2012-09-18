aaa0e53cc7752550de6d662eb8119a3752822486
gennadiiivanov <gennadii.ivanov@livestream.com>
2012-09-18 12:08:20 +0300

    added files

---
+++ prj/DoubleDate/Classes/ViewControllers/DDViewController+Design.h
@@ -0,0 +1,15 @@
+//
+//  DDViewController+Design.h
+//  DoubleDate
+//
+//  Created by Gennadii Ivanov on 9/5/12.
+//  Copyright (c) 2012 Gennadii Ivanov. All rights reserved.
+//
+
+#import "DDViewController.h"
+
+@interface DDViewController (Design)
+
+- (void)customize;
+
+@end
