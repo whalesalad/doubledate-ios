4a6d02d1d719ba50b2a491074f6fd86cfc0d5ffd
Gennadii Ivanov <gennadiiivanov@Gennadiis-Mac-mini.local>
2012-11-13 17:38:42 +0200

    added extensions

---
+++ prj/DoubleDate/Classes/ViewControllers/UIViewController+Extensions.h
@@ -0,0 +1,88 @@
+//
+//  UIViewController+Extensions.h
+//  DoubleDate
+//
+//  Created by Gennadii Ivanov on 9/5/12.
+//  Copyright (c) 2012 Gennadii Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MBProgressHUD;
+@class DDAPIController;
+
+@interface UIViewController (HUD)
+
+@property(nonatomic, retain) MBProgressHUD *hud;
+
+#define DECLARE_HUD_WITH_PROPERTY(_CLASS_, _P_)\
+@implementation _CLASS_ (HUD)\
+- (void)setHud:(MBProgressHUD *)v\
+{\
+    if (v != _P_)\
+    {\
+        [_P_ release];\
+        _P_ = [v retain];\
+    }\
+}\
+- (MBProgressHUD*)hud\
+{\
+    return _P_;\
+}\
+@end
+
+- (void)showHudWithText:(NSString*)text animated:(BOOL)animated;
+- (void)hideHud:(BOOL)animated;
+- (BOOL)isHudExist;
+- (UIView*)viewForHud;
+- (void)showCompletedHudWithText:(NSString*)text;
+
+@end
+
+@interface UIViewController (API)
+
+@property(nonatomic, retain) DDAPIController *apiController;
+
+#define DECLARE_API_CONTROLLER_WITH_PROPERTY(_CLASS_, _P_)\
+@implementation _CLASS_ (API)\
+- (void)setApiController:(DDAPIController *)v\
+{\
+    if (v != _P_)\
+    {\
+        [_P_ release];\
+        _P_ = [v retain];\
+    }\
+}\
+- (DDAPIController*)apiController\
+{\
+    return _P_;\
+}\
+@end
+
+@end
+
+@interface UIViewController (Other)
+
+@property(nonatomic, retain) NSMutableArray *buffer;
+
+#define DECLARE_BUFFER_WITH_PROPERTY(_CLASS_, _P_)\
+@implementation _CLASS_ (Other)\
+- (void)setBuffer:(NSMutableArray *)v\
+{\
+    if (v != _P_)\
+    {\
+        [_P_ release];\
+        _P_ = [v retain];\
+    }\
+}\
+- (NSMutableArray*)buffer\
+{\
+    return _P_;\
+}\
+@end
+
+- (UIViewController*)viewControllerForClass:(Class)vcClass;
+
+- (UIView*)viewForHeaderWithMainText:(NSString*)mainText detailedText:(NSString*)detailedText;
+
+@end
---
+++ prj/DoubleDate/Classes/ViewControllers/UIViewController+Extensions.m
@@ -0,0 +1,222 @@
+//
+//  UIViewController+Extensions.m
+//  DoubleDate
+//
+//  Created by Gennadii Ivanov on 9/5/12.
+//  Copyright (c) 2012 Gennadii Ivanov. All rights reserved.
+//
+
+#import "UIViewController+Extensions.h"
+#import "MBProgressHUD.h"
+#import "DDAPIController.h"
+
+@implementation UIViewController (HUD)
+
+- (void)setHud:(MBProgressHUD*)hud
+{
+}
+
+- (MBProgressHUD*)hud
+{
+    return nil;
+}
+
+- (UIView*)viewForHud
+{
+    NSArray *windows = [UIApplication sharedApplication].windows;
+    return [windows objectAtIndex:[windows count]-1];
+}
+
+- (MBProgressHUD*)HUDForView:(UIView*)view
+{
+    return self.hud;
+}
+
+- (void)showHudWithText:(NSString*)text animated:(BOOL)animated
+{
+    //get hud
+    MBProgressHUD *hud = [self HUDForView:[self viewForHud]];
+    
+    //check if we should hide first
+    if (hud && animated)
+    {
+        //hide hud
+        [self hideHud:YES];
+        hud = nil;
+        
+        //unset own hud
+        self.hud = nil;
+    }
+    
+    //check if we should just change a text
+    if (hud && !animated)
+        hud.labelText = text;
+    
+    //check if no hud
+    if (!hud)
+    {
+        //add hud
+        hud = [[[MBProgressHUD alloc] initWithView:[self viewForHud]] autorelease];
+        hud.dimBackground = YES;
+        hud.labelText = text;
+        [[self viewForHud] addSubview:hud];
+        [hud show:animated];
+        
+        //save own hud
+        self.hud = hud;
+    }
+    
+    //bring to parent
+    [hud.superview bringSubviewToFront:hud];
+}
+
+- (void)hideHud:(BOOL)animated
+{
+    //get hud
+    MBProgressHUD *hud = [self HUDForView:[self viewForHud]];
+    
+    //remove hud
+    hud.removeFromSuperViewOnHide = YES;
+    [hud hide:animated];
+    
+    //unset own hud
+    self.hud = nil;
+}
+
+- (BOOL)isHudExist
+{
+    return [self HUDForView:[self viewForHud]] != nil;
+}
+
+- (void)showCompletedHudWithText:(NSString *)text
+{
+    //add hud
+    MBProgressHUD *hud = [[[MBProgressHUD alloc] initWithView:[self viewForHud]] autorelease];
+    hud.customView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"37x-Checkmark.png"]] autorelease];
+    hud.mode = MBProgressHUDModeCustomView;
+    hud.labelText = text;
+    [[self viewForHud] addSubview:hud];
+    [hud show:YES];
+    [hud hide:YES afterDelay:2];
+}
+
+@end
+
+@implementation UIViewController (API)
+
+- (void)setApiController:(DDAPIController*)apiController
+{
+}
+
+- (DDAPIController*)apiController
+{
+    return nil;
+}
+
+@end
+
+@implementation UIViewController (Other)
+
+- (void)setBuffer:(NSMutableArray*)v
+{
+}
+
+- (NSMutableArray*)buffer
+{
+    return nil;
+}
+
+- (UIViewController*)viewControllerForClass:(Class)vcClass inViewController:(UIViewController*)vc
+{
+    //check dummy
+    if (!vc)
+        return nil;
+    
+    //check if already checked
+    for (NSNumber *number in self.buffer)
+    {
+        if ([number unsignedIntegerValue] == [vc hash])
+            return nil;
+    }
+    
+    //mark as checked
+    [self.buffer addObject:[NSNumber numberWithUnsignedInteger:[vc hash]]];
+    
+    //check self
+    if ([vc isKindOfClass:vcClass])
+        return vc;
+    
+    //init value
+    UIViewController *ret = nil;
+    
+    //check parent
+    ret = [self viewControllerForClass:vcClass inViewController:vc.parentViewController];
+    if (ret)
+        return ret;
+    
+    //check presented
+    ret = [self viewControllerForClass:vcClass inViewController:vc.presentedViewController];
+    if (ret)
+        return ret;
+    
+    //check presenting
+    ret = [self viewControllerForClass:vcClass inViewController:vc.presentingViewController];
+    if (ret)
+        return ret;
+    
+    //check navigation controller
+    if ([vc isKindOfClass:[UINavigationController class]])
+    {
+        UINavigationController *nc = (UINavigationController*)vc;
+        for (UIViewController *v in nc.viewControllers)
+        {
+            ret = [self viewControllerForClass:vcClass inViewController:v];
+            if (ret)
+                return ret;
+        }
+    }
+    
+    return nil;
+}
+
+- (UIViewController*)viewControllerForClass:(Class)vcClass
+{
+    self.buffer = [[NSMutableArray alloc] init];
+    UIViewController *ret = [self viewControllerForClass:vcClass inViewController:self];
+    self.buffer = nil;
+    return ret;
+}
+
+- (UIView*)viewForHeaderWithMainText:(NSString*)mainText detailedText:(NSString*)detailedText
+{
+    //set general view
+    UIView *view = [[[UIView alloc] initWithFrame:CGRectMake(0, 0, 200, 44)] autorelease];
+    view.backgroundColor = [UIColor clearColor];
+    
+    //add label
+    UILabel *labelMain = [[[UILabel alloc] initWithFrame:CGRectZero] autorelease];
+    DD_F_HEADER_MAIN(labelMain);
+    labelMain.textColor = [UIColor grayColor];
+    labelMain.text = mainText;
+    [labelMain sizeToFit];
+    labelMain.frame = CGRectMake(22, 18, labelMain.frame.size.width, labelMain.frame.size.height);
+    labelMain.backgroundColor = [UIColor clearColor];
+    [view addSubview:labelMain];
+    
+    //add label
+    if ([detailedText length])
+    {
+        UILabel *labelDetailed = [[[UILabel alloc] initWithFrame:CGRectZero] autorelease];
+        DD_F_HEADER_DETAILED(labelDetailed);
+        labelDetailed.textColor = [UIColor grayColor];
+        labelDetailed.text = detailedText;
+        [labelDetailed sizeToFit];
+        labelDetailed.frame = CGRectMake(labelMain.frame.origin.x+labelMain.frame.size.width+8, labelMain.frame.origin.y+2, labelDetailed.frame.size.width, labelMain.frame.size.height);
+        labelDetailed.backgroundColor = [UIColor clearColor];
+        [view addSubview:labelDetailed];
+    }
+    
+    return view;
+}
+
+@end
