cdf0c287d6a58455323a224cd4a61eba30933a7a
Michael Whalen <michael@whalesalad.com>
2013-01-22 13:11:42 -0500

    Removed the old icon, added new one in.

--- prj/DoubleDate/Classes/Other/DDTools.m
+++ prj/DoubleDate/Classes/Other/DDTools.m
@@ -10,8 +10,8 @@
 #import "SBJson.h"
 #import <QuartzCore/QuartzCore.h>
 
-#define API_URL @"http://localhost:3000"
-//#define API_URL @"http://api-2.dbld8.com"
+//#define API_URL @"http://localhost:3000"
+#define API_URL @"http://api-2.dbld8.com"
 #define SERVER_URL @"http://dbld8.com"
 
 @implementation DDTools
