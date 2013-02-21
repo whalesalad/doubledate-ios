7bb863a91e801c86928f570b3996c9d78a579c90
Michael Whalen <michael@whalesalad.com>
2013-02-21 18:09:08 +0100

    Minor text changes.

--- prj/DoubleDate/Classes/ViewControllers/DDWelcomeViewController.m
+++ prj/DoubleDate/Classes/ViewControllers/DDWelcomeViewController.m
@@ -78,8 +78,8 @@
 - (void)cutomizePrivacyTextView
 {
     //set text
-    NSString *title = NSLocalizedString(@"Your Privacy is Improtant!", nil);
-    NSString *message = NSLocalizedString(@"We rely on Facebook to ensure That DoubleDate are genuine. We'll never post on your wall, or spam you friends. Promise!", nil);
+    NSString *title = NSLocalizedString(@"Your Privacy is Important", nil);
+    NSString *message = NSLocalizedString(@"We rely on Facebook to ensure That DoubleDaters are genuine. We'll never post on your wall, or spam you friends.", nil);
     NSString *fullText = [NSString stringWithFormat:@"%@\n%@", title, message];
     
     //create attributed text
