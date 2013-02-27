316796a6b586f4925d8b6b8402a0a12acad06b12
Gennadii Ivanov <gennadiiivanov@Gennadiis-Mac-mini.local>
2013-02-27 18:49:38 +0200

    added files

---
+++ prj/DoubleDate/Classes/Views/DDCoinsBar.h
@@ -0,0 +1,17 @@
+//
+//  DDCoinsBar.h
+//  DoubleDate
+//
+//  Created by Gennadii Ivanov on 9/17/12.
+//  Copyright (c) 2012 Gennadii Ivanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DDCoinsBar : UIControl
+{
+}
+
+- (void)setValue:(NSInteger)value;
+
+@end
---
+++ prj/DoubleDate/Classes/Views/DDCoinsBar.m
@@ -0,0 +1,91 @@
+//
+//  DDCoinsBar.m
+//  DoubleDate
+//
+//  Created by Gennadii Ivanov on 9/17/12.
+//  Copyright (c) 2012 Gennadii Ivanov. All rights reserved.
+//
+
+#import "DDCoinsBar.h"
+#import "DDTools.h"
+
+@interface DDCoinsBar()
+
+@property(nonatomic, retain) IBOutlet UIButton *buttonMoreCoins;
+@property(nonatomic, retain) IBOutlet UIView *labelCoinsContainer;
+@property(nonatomic, retain) IBOutlet UIImageView *imageViewCoins;
+@property(nonatomic, retain) IBOutlet UILabel *labelCoins;
+
+@end
+
+@implementation DDCoinsBar
+
+@synthesize buttonMoreCoins;
+@synthesize labelCoinsContainer;
+@synthesize imageViewCoins;
+@synthesize labelCoins;
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+    
+    //update more coins button
+    [self.buttonMoreCoins setBackgroundImage:[DDTools resizableImageFromImage:[self.buttonMoreCoins backgroundImageForState:UIControlStateNormal]] forState:UIControlStateNormal];
+}
+
+- (void)alignCoinsLabel
+{
+    //save distance between label and button
+    CGFloat gap = (self.labelCoins.frame.origin.x - self.imageViewCoins.frame.origin.x - self.imageViewCoins.frame.size.width);
+    
+    //move label coins
+    CGSize newLabelSize = CGSizeMake([self.labelCoins sizeThatFits:self.labelCoins.bounds.size].width, self.labelCoins.frame.size.height);
+    
+    //update center of the label
+    self.labelCoins.frame = CGRectMake(0, self.labelCoins.frame.origin.y, newLabelSize.width, self.labelCoins.frame.size.height);
+    self.labelCoins.center = CGPointMake(self.labelCoinsContainer.frame.size.width / 2 + (gap + self.imageViewCoins.frame.size.width) / 2, self.labelCoins.center.y);
+    
+    //update center of image view coins
+    self.imageViewCoins.center = CGPointMake(self.labelCoins.frame.origin.x - gap - self.imageViewCoins.frame.size.width / 2, self.imageViewCoins.center.y);
+}
+
+- (void)setValue:(NSInteger)value
+{
+    self.labelCoins.text = [NSString stringWithFormat:@"%d", value];
+    [self alignCoinsLabel];
+}
+
+- (void)addTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents
+{
+    [self.buttonMoreCoins addTarget:target action:action forControlEvents:controlEvents];
+}
+
+- (void)removeTarget:(id)target action:(SEL)action forControlEvents:(UIControlEvents)controlEvents
+{
+    [self.buttonMoreCoins removeTarget:target action:action forControlEvents:controlEvents];
+}
+
+- (NSSet *)allTargets
+{
+    return [self.buttonMoreCoins allTargets];
+}
+
+- (UIControlEvents)allControlEvents
+{
+    return [self.buttonMoreCoins allControlEvents];
+}
+- (NSArray *)actionsForTarget:(id)target forControlEvent:(UIControlEvents)controlEvent
+{
+    return [self.buttonMoreCoins actionsForTarget:target forControlEvent:controlEvent];
+}
+
+- (void)dealloc
+{
+    [buttonMoreCoins release];
+    [labelCoinsContainer release];
+    [imageViewCoins release];
+    [labelCoins release];
+    [super dealloc];
+}
+
+@end
