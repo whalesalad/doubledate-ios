87b6e6708e69371c88e90803f14f52b42acb80f0
Gennadii Ivanov <gennadii@Mac-mini-Gennadii.local>
2013-03-04 23:25:55 +0200

    added store kit products extraction

---
+++ prj/DoubleDate/Classes/Controllers/DDStoreKitController.h
@@ -0,0 +1,35 @@
+//
+//  DDStoreKitController.h
+//  DoubleDate
+//
+//  Created by Gennadii Ivanov on 9/5/12.
+//  Copyright (c) 2012 Gennadii Ivanov. All rights reserved.
+//
+
+#import <StoreKit/StoreKit.h>
+
+@protocol DDStoreKitControllerDelegate
+
+@optional
+
+- (void)productsReceived:(NSArray*)products;
+- (void)productsReceivingFailed:(NSError*)error;
+
+@end
+
+@interface DDStoreKitController : NSObject<SKProductsRequestDelegate, SKPaymentTransactionObserver>
+{
+	id<DDStoreKitControllerDelegate> delegate_;
+	SKProductsRequest *request_;
+	NSArray *products_;
+}
+
+@property(assign) id<DDStoreKitControllerDelegate> delegate;
+
++ (DDStoreKitController *)sharedController;
+
+- (void)requestProductDataWithPids:(NSSet*)pids;
+
++ (NSString*)localizedPriceFromProduct:(SKProduct*)product;
+
+@end
