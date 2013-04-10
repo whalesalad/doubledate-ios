496d9156ce6098749e0edc5265012f184dc1ad66
Gennadii Ivanov <gennadii@Mac-mini-Gennadii.local>
2013-04-11 00:44:13 +0300

    fixed image orientation bug

---
+++ prj/DoubleDate/Classes/Other/UIImage+DD.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+DD.h
+//  DoubleDate
+//
+//  Created by Gennadii Ivanov 
+//  Copyright (c) 2012-2013 Belluba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (DD)
+
+- (UIImage *)fixOrientation;
+
+@end
